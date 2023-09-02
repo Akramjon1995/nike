76fecc7ad6c1ec7bb9b4caf7e0bf08f0373848fe
Akramjon Akramov <akramjonakramov96.com>
2023-09-02 12:04:53 +0500

    buiding main sections

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Footer from "./Footer";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import PopularProducts from "./PopularProducts";
+import CustomerReviews from "./Customerreviews";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Hero from "./Hero";
+
+export {
+    Hero,
+    Footer,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    PopularProducts,
+    CustomerReviews
+}
